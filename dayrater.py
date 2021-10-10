8b3f81fd94fd51ffee2121e486cfdb7279acd3e2
GOGlMAn <jszymczak887@gmail.com>
2021-10-10 12:06:41 +0200

    Add files via upload

---
+++ dayrater.py
@@ -0,0 +1,3 @@
+#dayrate
+rating = int(input("Oceń swój dzień od 0 do 10: "))
+print(rating)
---
+++ przywitanie.py
@@ -0,0 +1,5 @@
+#name and age
+#przywitanie
+name = str(input("Jak masz na imie? "))
+age = int(input("Ile masz lat? "))
+print("Twoje imie to " + name + " i masz", age, "lat")
